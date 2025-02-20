b7bfcbbe1e91d40fea4533a43bcd5d58b7a6cd96
hujinyou <hujinyou@itiger.com>
2025-02-21 00:22:30 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const Projects = [
+    {
+        name: 'Home',
+        description: '简单生活，深刻思考，让每一步都充满意义。',
+        image: 'images/project1.jpg'
+    },
+    {
+        name: 'Max Vision',
+        description: '突破视觉的极限，锻炼色彩辨识力，让每一处细微差异无所遁形。',
+        image: 'images/project1.jpg',
+        url: 'https://jyhu.github.io/MaxVision'
+    }
+]
