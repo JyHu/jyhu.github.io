ba30129cf3c374691647ea7a6aa2244cbc7136ea
hujinyou <hujinyou@itiger.com>
2025-11-14 10:21:00 +0800

    add transx tab

--- js/config.js
+++ js/config.js
@@ -9,5 +9,11 @@ const Projects = [
         description: '突破视觉的极限，锻炼色彩辨识力，让每一处细微差异无所遁形。',
         image: 'images/project1.jpg',
         url: 'https://jyhu.github.io/MaxVision'
+    },
+    {
+        name: "TransX",
+        description: '轻松应对本地化挑战，一站式多语言管理方案。',
+        image: 'images/project1.jpg',
+        url: 'https://jyhu.github.io/TransXHelp'
     }
 ]
