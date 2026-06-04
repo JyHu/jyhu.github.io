e2800557b01746f3cdc20be555c5757a5a897cae
hujinyou <hujinyou@itiger.com>
2026-06-04 23:09:59 +0800

    reset host

--- assets/js/config.js
+++ assets/js/config.js
@@ -8,12 +8,12 @@ const Projects = [
         name: 'Max Vision',
         description: '突破视觉的极限，锻炼色彩辨识力，让每一处细微差异无所遁形。',
         image: 'images/project1.jpg',
-        url: 'https://auu.space/MaxVision'
+        url: 'https://yunxi.cyou/MaxVision'
     },
     {
         name: "TransX",
         description: '轻松应对本地化挑战，一站式多语言管理方案。',
         image: 'images/project1.jpg',
-        url: 'https://auu.space/X'
+        url: 'https://yunxi.cyou/TransX'
     }
 ]
